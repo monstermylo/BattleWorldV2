e7ba01a3fcd96b703d50397530c7e1bf4ee13ca2
monstermylo <myles.selvey@sky.com>
2018-09-24 23:15:41 +0100

    v0.1.1 card access singleton
    moved get and set userOwned and get userDeck to card access

--- src/cardAccess.js
+++ src/cardAccess.js
@@ -3,14 +3,63 @@ var cardAccess = (function () {
     var instance;
 
     function init() {
+
+        //private variables
+        var privateVariable = "private2";
+
+
+        //private methods
         function privateMethod() {
             console.log("private");
         }
-        var privateVariable = "private2";
 
+
+        function getUserOwned() {
+            var userOwned2 = new Array();
+            userOwned2 = localStorage.getItem("ownedCards");
+            userOwned2 = JSON.parse(userOwned2);
+            console.log("userowned");
+            console.log(userOwned2);
+
+            return userOwned2;
+
+        }
+
+        function setUserOwned(userOwned) {
+            console.log("userOwned");
+            console.log(userOwned);
+            localStorage.setItem("ownedCards", JSON.stringify(userOwned));
+
+        }
+        
+        function getUserDeckPrivate() {
+            var userDeck2 = new Array();
+            userDeck2 = localStorage.getItem("userDeck");
+            userDeck2 = JSON.parse(userDeck2);
+            console.log("userDeck");
+            console.log(userDeck2);
+
+            return userDeck2;
+
+        }
+
+        //public methods
         return {
             getOwnedPublic: function () {
-                console.log("getOwned public");
+                console.log("getOwnedPublic");
+                var userOwned2 = Array();
+                userOwned2 = getUserOwned()
+                return userOwned2;
+            },
+            setOwnedPublic: function (userOwned) {
+                console.log("setOwnedPublic");
+                setUserOwned(userOwned);
+            },
+            getUserDeck: function (){
+                console.log("getUserDeck");
+                var userDeck2 = Array();
+                userDeck2 = getUserDeckPrivate();
+                return userDeck2;
             }
         };
 
--- src/deckset.js
+++ src/deckset.js
@@ -8,20 +8,18 @@ class deckset extends Phaser.Scene {
 
 
     preload() {
+        var cardAccessI = cardAccess.getInstance();
         var userdeck = Array();
-        userdeck = localStorage.getItem("userDeck");
-        userdeck = JSON.parse(userdeck);
-        console.log("userdeck");
+        userdeck = cardAccessI.getUserDeck("userDeck");
+
         if (userdeck == null) {
             userdeck = {};
         }
-        console.log(userdeck);
+
         var userOwned = Array();
-        userOwned = localStorage.getItem("ownedCards");
-        //console.log(userOwned);
-        userOwned = JSON.parse(userOwned);
-        console.log("userowned");
-        console.log(userOwned);
+        var userOwned = cardAccessI.getOwnedPublic();
+
+
         var x;
         var y = userOwned.length;
         for (x = 0; x < y; x++) {
@@ -30,11 +28,10 @@ class deckset extends Phaser.Scene {
 
         this.load.image('menu', 'assets/menu.png');
         this.load.image('story', 'assets/story.png');
-
-        console.log(userOwned);
     }
 
     create() {
+        var cardAccessI = cardAccess.getInstance();
         this.text = this.add.text(0, 0, "deckset", {
             font: "40px Impact"
         });
@@ -50,32 +47,22 @@ class deckset extends Phaser.Scene {
         var card = new Array();
         var currentCard = new Array();
         var newCard = new Array();
-        console.log(userOwned);
+
         var userOwned = Array();
+        userOwned = cardAccessI.getOwnedPublic();
 
-        console.log("userowned");
-        console.log(userOwned);
-
-        userOwned = localStorage.getItem("ownedCards");
-        userOwned = JSON.parse(userOwned);
-        console.log("userowned");
-        console.log(userOwned);
 
         var userdeck = Array();
-        userdeck = localStorage.getItem("userDeck");
-        userdeck = JSON.parse(userdeck);
-        console.log("userdeck");
-        console.log(userdeck);
+        userdeck = cardAccessI.getUserDeck();
+
         if (userdeck == null) {
             userdeck = [];
         }
-        console.log("userdeck");
-        console.log(userdeck);
 
         var x;
         var y = userOwned.length;
         for (x = 0; x < y; x++) {
-            console.log(userOwned[x].name);
+
             //name of card in owned row
             this.text2 = this.add.text(screenWidth * (4 / 5) + (x * 70) - 70, screenHeight * (3.9 / 5) * (3 / scaleRatio), userOwned[x].name, {
                 font: "40px Impact"
@@ -88,96 +75,69 @@ class deckset extends Phaser.Scene {
 
         }
 
-        console.log("userowned");
-        console.log(userOwned);
+
 
         var x = 0;
         var y = userdeck.length;
-        console.log("userdeck");
-        console.log(userdeck);
+
         for (x = 0; x < y; x++) {
             //console.log(x);
             this.text2 = this.add.text(screenWidth * (4 / 5) + (x * 70) - 70, screenHeight * (2.9 / 5) * (3 / scaleRatio), userdeck[x].name, {
                 font: "40px Impact"
             });
 
-            //console.log(scaleRatio);
-
             card[x] = this.add.image(screenWidth * (4 / 5) + (x * 70), screenHeight * (3.5 / 5) * (3 / scaleRatio), userdeck[x].name).setInteractive();
 
             currentCard = card[x];
 
-            //console.log(currentCard);
         }
 
-        console.log("userowned");
-        console.log(userOwned);
 
         this.input.on('gameobjectdown', function (pointer, gameObject) {
-            var cardAccessI = cardAccess.getInstance();
-            console.log(cardAccessI);
-            cardAccessI.getOwnedPublic();
-            var userOwned2 = Array();
-            userOwned2 = localStorage.getItem("ownedCards");
-            userOwned2 = JSON.parse(userOwned2);
-            console.log("userowned");
-            console.log(userOwned2);
+            //var cardAccessI = cardAccess.getInstance();     
+            var userOwned = Array();
+            var userOwned = cardAccessI.getOwnedPublic();
 
             var key1 = gameObject.texture.key;
             var key2 = key1.slice(4);
-            console.log(key1);
-            console.log(key2);
-            console.log(parseInt(key2, 10));
 
             if (parseInt(key2, 10) != null) {
                 console.log("not null");
             } else {
                 console.log("is null");
             }
-            console.log(userOwned2);
+
             if (!isNaN(parseInt(key2, 10))) {
-                console.log(userOwned2);
+
                 var newcard = new Array();
-                for (var x = 0; x < userOwned2.length; x++) {
-                    if (userOwned2[x].name = key1) {
-                        newcard = userOwned2[x];
+                for (var x = 0; x < userOwned.length; x++) {
+                    if (userOwned[x].name = key1) {
+                        newcard = userOwned[x];
                         var cardKey = x;
 
                     }
                 }
-                console.log(userOwned2);
-                //console.log(newCard);
-                console.log(userdeck);
-                console.log(userdeck.length);
+
                 if (userdeck.length < 10 || !userdeck.length) {
-                    console.log(newcard);
-                    console.log(userOwned2);
+
                     if (userdeck === []) {
                         userdeck = newcard;
                     } else {
                         userdeck.push(newcard);
                     }
 
-                    //userdeck.push(newCard);
-                    console.log(userdeck);
-                    //userOwned.splice(cardKey, 1);
                     var userOwned1 = new Array();
-                    for (var x = 0; x < userOwned2.length; x++) {
-                        console.log(userOwned2[x].name);
-                        console.log(userdeck[0].name);
-                        if (userOwned2[x].name != newcard.name) {
-                            userOwned1.push(userOwned2[x]);
-                            console.log(userOwned1);
+                    for (var x = 0; x < userOwned.length; x++) {
+
+                        if (userOwned[x].name != newcard.name) {
+                            userOwned1.push(userOwned[x]);
                         }
                     }
 
                     userOwned = userOwned1;
 
-                    console.log("userowned");
-                    console.log(userOwned);
-                    localStorage.setItem("ownedCards", JSON.stringify(userOwned));
-                    console.log("userdeck");
-                    console.log(userdeck);
+                    cardAccessI.setOwnedPublic(userOwned);
+
                     localStorage.setItem("userDeck", JSON.stringify(userdeck));
 
                     console.log("reload");
@@ -197,42 +157,31 @@ class deckset extends Phaser.Scene {
     }
 
     update() {
+        var cardAccessI = cardAccess.getInstance();
         this.clear = this.add.image(800, 300, 'story').setScale(0.2).setInteractive();
         this.clear.on('pointerdown', function (event) {
 
             var userOwned = Array();
-            userOwned = localStorage.getItem("ownedCards");
-            userOwned = JSON.parse(userOwned);
-            console.log("userowned");
-            console.log(userOwned);
+            var userOwned = cardAccessI.getOwnedPublic();
 
             var userdeck = Array();
-            userdeck = localStorage.getItem("userDeck");
-            userdeck = JSON.parse(userdeck);
-            console.log("userdeck");
-            console.log(userdeck);
+            userdeck = cardAccessI.getUserDeck("userDeck");
+
             if (userdeck == null) {
                 userdeck = new Array();
             }
-            console.log("userdeck");
-            console.log(userdeck);
 
             var deckCard = new Array();
 
             var deckLength = userdeck.length;
             var x = 0;
             for (x = 0; x < deckLength; x++) {
-                deckCard = userdeck[x];
-                console.log(x);
-                console.log(deckCard);
+                deckCard = userdeck[x]
                 userOwned.push(deckCard);
-                console.log(userOwned);
             }
 
-            console.log("userowned");
-            console.log(userOwned);
 
-            localStorage.setItem("ownedCards", JSON.stringify(userOwned));
+            cardAccessI.setOwnedPublic(userOwned);
 
             localStorage.removeItem("userDeck");
             this.scene.start("deckset");
